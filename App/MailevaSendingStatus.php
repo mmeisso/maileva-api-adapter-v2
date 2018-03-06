76b2a68c1a0cefe8e343cb8815cc8b85884d4fa1
loic <lpettiti@eukles.com>
2018-03-06 14:26:49 +0100

    Fix default mail

---
+++ App/MailevaSendingStatus.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MailevaApiAdapter\App;
+
+/**
+ * Class MailevaSendingStatus
+ * @package MailevaApiAdapter\App
+ */
+class MailevaSendingStatus
+{
+    const DRAFT = "DRAFT";
+    const PAYMENT_REQUIRED = "PAYMENT_REQUIRED";
+    const PENDING = "PENDING";
+    const ACCEPTED = "ACCEPTED";
+    const REJECTED = "REJECTED";
+    const PROCESSED = "PROCESSED";
+    const PROCESSED_WITH_ERRORS = "PROCESSED_WITH_ERRORS";
+
+}
