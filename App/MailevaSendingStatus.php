d927920f1809764830cbcb10bcde65ea600501ed
damien <drousseaux@eukles.com>
2019-02-21 15:46:25 +0100

    Add status CANCELED and REFUNDED

--- App/MailevaSendingStatus.php
+++ App/MailevaSendingStatus.php
@@ -8,15 +8,16 @@ namespace MailevaApiAdapter\App;
  */
 class MailevaSendingStatus
 {
+    const ACCEPTED = "ACCEPTED";
+    const CANCELED = "CANCELED";
     const DRAFT = "DRAFT";
+    const ERROR = "ERROR";
+    const FULL_PROCESSED_LRE = "FULL_PROCESSED_LRE";
     const PAYMENT_REQUIRED = "PAYMENT_REQUIRED";
     const PENDING = "PENDING";
-    const ACCEPTED = "ACCEPTED";
-    const REJECTED = "REJECTED";
     const PROCESSED = "PROCESSED";
-    const FULL_PROCESSED_LRE = "FULL_PROCESSED_LRE";
     const PROCESSED_WITH_ERRORS = "PROCESSED_WITH_ERRORS";
-    const ERROR = "ERROR";
+    const REFUNDED = 'REFUNDED';
+    const REJECTED = "REJECTED";
     const SUBMIT_ERROR = "SUBMIT_ERROR";
-
 }
